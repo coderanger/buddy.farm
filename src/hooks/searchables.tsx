7481fa066f876310098d51d6df3553dcd3753bc8
Noah Kantrowitz <noah@coderanger.net>
2022-03-06 16:13:57 -0800

    ✨ A working (but bad) search implementation.

---
+++ src/hooks/searchables.tsx
@@ -0,0 +1,56 @@
+import { useStaticQuery, graphql } from "gatsby"
+
+interface SearchablesQuery {
+  locations: {
+    nodes: {
+      name: string
+      image: string
+    }[]
+  }
+  items: {
+    nodes: {
+      name: string
+      image: string
+    }[]
+  }
+}
+
+interface Searchable {
+  name: string
+  image: string
+  href: string
+}
+
+const nodeToSearchable = (node: { name: string, image: string }) => {
+  const searchName = node.name.toLowerCase()
+  return { name: searchName, image: node.image, href: `/${searchName.replace(/\s+/g, '-')}/` }
+}
+
+export const useSearchables = () => {
+  const { locations, items }: SearchablesQuery = useStaticQuery(
+    graphql`
+    query {
+      locations: allLocationsJson {
+        nodes {
+          name
+          image
+        }
+      }
+      items: allItemsJson {
+        nodes {
+          name
+          image
+        }
+      }
+    }
+    `
+  )
+  const searchables: Searchable[] = []
+  for (const node of locations.nodes) {
+    searchables.push(nodeToSearchable(node))
+  }
+  for (const node of items.nodes) {
+    searchables.push(nodeToSearchable(node))
+  }
+  return searchables
+}
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,12 +1,23 @@
 import { navigate } from 'gatsby';
+import ListGroup from "react-bootstrap/ListGroup"
 import { useEffect, useState } from 'react';
 
 import Layout from '../components/layout';
+import { useSearchables } from '../hooks/searchables';
+
+interface ScoredResult {
+  name: string
+  image: string
+  href: string
+  score: number
+}
 
 export default () => {
+  const searchables = useSearchables()
   const [query, setQuery] = useState<string | undefined>(undefined)
   const inBrowser = typeof document !== 'undefined'
 
+  // Get the search query from the URL.
   // Based on https://github.com/akash-joshi/gatsby-query-params/blob/f997c33cdee82d053c6591ff3b71b7d54cce07d3/src/index.js
   useEffect(() => {
     if (inBrowser) {
@@ -25,7 +36,35 @@ export default () => {
     navigate(`/search/?q=${encodeURIComponent(query)}`, { replace: true })
   }
 
+  // Filter and sort the results.
+  const [results, setResults] = useState<ScoredResult[]>([])
+  useEffect(() => {
+    if (!query) {
+      setResults([])
+      return
+    }
+    // Transform to a scored list.
+    const scored: ScoredResult[] = []
+    for (const { name, image, href } of searchables) {
+      // TODO better search
+      const score = name.includes(query.toLowerCase()) ? 1 : 0
+      if (score > 0) {
+        scored.push({ name, image, href, score })
+      }
+    }
+    scored.sort((a, b) => a.score - b.score)
+    setResults(scored)
+  }, [query])
+
   return <Layout pageTitle="Buddy's Almanac" query={query} onSearch={onSearch}>
     <div>Search results</div>
+    <ListGroup variant="flush">
+      {results.map(result => (
+        <ListGroup.Item className="d-flex w-100 justify-content-between" onClick={evt => { evt.preventDefault(); navigate(result.href) }}>
+          <img src={"https://farmrpg.com" + result.image} className="d-inline-block align-text-top" width="48" height="48" css={{ marginRight: 10, boxSizing: "border-box" }} />
+          <span css={{ fontSize: 32 }}>{result.name}</span>
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
   </Layout>
 }
