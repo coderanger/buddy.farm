e875f617ed8c764063019b29ea2081ff83a1c95e
Noah Kantrowitz <noah@coderanger.net>
2022-03-06 16:22:36 -0800

    ✨ Basic index content for now.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,16 @@ import Layout from '../components/layout'
 
 export default () => (
   <Layout pageTitle="Buddy's Almanac">
-    <div>TODO</div>
+    <h1>Welcome to Buddy's Almanac</h1>
+    <div>
+      <p>This is a repository of game information for <a href="https://farmrpg.com/">FarmRPG</a>.</p>
+      <p>
+        We're still getting set up so please be patient as new features are added. For now you can look up drop rates for
+        items and locations. This does not yet include manual fishing or post-Iron Depot change rates, but it will soon.
+      </p>
+      <p>
+        Please contact Coderanger in-game or on Discord with any questions or feedback.
+      </p>
+    </div>
   </Layout>
 )
