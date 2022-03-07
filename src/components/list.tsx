10fedc7b166cb2771298083c10fd846fabb7d6c4
Noah Kantrowitz <noah@coderanger.net>
2022-03-06 16:13:31 -0800

    🎨 Format.

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -15,19 +15,19 @@ interface ListProps {
   items: ListItem[]
 }
 
-export default ({items} : ListProps) => (
-    <ListGroup variant="flush">
+export default ({ items }: ListProps) => (
+  <ListGroup variant="flush">
     {items.map((item: ListItem) => {
       const href = item.href || (item.hrefSlugify && `/${item.hrefSlugify.toLowerCase().replace(/\s+/g, '-')}/`)
-      return <ListGroup.Item className="d-flex w-100 justify-content-between" onClick={evt => {evt.preventDefault(); href && navigate(href)}}>
+      return <ListGroup.Item className="d-flex w-100 justify-content-between" onClick={evt => { evt.preventDefault(); href && navigate(href) }}>
         <div>
-          <img src={"https://farmrpg.com" + item.image} className="d-inline-block align-text-top" width="48" height="48" css={{marginRight: 10, boxSizing: "border-box"}} />
+          <img src={"https://farmrpg.com" + item.image} className="d-inline-block align-text-top" width="48" height="48" css={{ marginRight: 10, boxSizing: "border-box" }} />
           <div className="d-inline-block align-text-top">
-            <div css={{fontWeight: "bold"}}>{item.lineOne}</div>
+            <div css={{ fontWeight: "bold" }}>{item.lineOne}</div>
             <div>{item.lineTwo}</div>
           </div>
         </div>
-        <div css={{fontSize: 32}}>{item.value}</div>
+        <div css={{ fontSize: 32 }}>{item.value}</div>
       </ListGroup.Item>
     })}
   </ListGroup>
