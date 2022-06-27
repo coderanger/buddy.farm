83f6538ec5f422e7751d1f662d4e864f7bacc09d
Noah Kantrowitz <noah@coderanger.net>
2022-06-26 23:01:12 -0700

    🐛 Correctly optional args.

--- src/hooks/servertime.tsx
+++ src/hooks/servertime.tsx
@@ -7,7 +7,7 @@ export interface UseServerTimeProps {
 
 const getServerTime = () => DateTime.now().setZone("America/Chicago").toLocaleString(DateTime.TIME_SIMPLE)
 
-const getRollover = (opts: UseServerTimeProps) => {
+const getRollover = (opts: UseServerTimeProps | undefined) => {
   const rollover = DateTime.fromObject({}, { zone: "America/Chicago" }).startOf("day").plus({ day: 1 })
   let delta = rollover.diffNow().shiftTo("hours", "minutes", "seconds").normalize()
   if (delta.hours === 0) {
@@ -18,13 +18,13 @@ const getRollover = (opts: UseServerTimeProps) => {
   }
   // Round down seconds. Otherwise things like seconds=59.9 shows as 60 and it looks silly.
   delta = delta.set({ seconds: delta.seconds - (delta.seconds % 1) })
-  if (opts.exchangeCenter && delta.hours >= 12) {
+  if (opts?.exchangeCenter && delta.hours >= 12) {
     delta = delta.set({ hours: delta.hours - 12 })
   }
   return delta.toHuman({ maximumFractionDigits: 0 })
 }
 
-export const useServerTime = (opts: UseServerTimeProps): [string, string] => {
+export const useServerTime = (opts: UseServerTimeProps | undefined): [string, string] => {
   const [time, setTime] = useState(getServerTime)
   const [rollover, setRollover] = useState(() => getRollover(opts))
 
