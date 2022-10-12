72bd5ef08622bbdc7c0a480c07931c909b605f1a
Noah Kantrowitz <noah@coderanger.net>
2022-10-11 17:47:52 -0700

    🎨 Reformat.

--- src/pages/towercalc.tsx
+++ src/pages/towercalc.tsx
@@ -1,10 +1,7 @@
-import React, { useContext, useEffect, useState } from 'react'
-
-import { Calculator } from '../components/calculator'
-import { Input } from '../components/input'
-import { GlobalContext } from '../utils/context'
-import ButtonGroup from 'react-bootstrap/ButtonGroup'
-import Form from 'react-bootstrap/Form'
+import { Calculator } from "../components/calculator"
+import { Input } from "../components/input"
+import ButtonGroup from "react-bootstrap/ButtonGroup"
+import React from "react"
 
 interface TowerData {
   fromLevel: number
@@ -25,18 +22,18 @@ const DEFAULT_DATA: TowerData = {
 }
 
 export default () => {
-  const [data, values, setValues] = Calculator.useData(DEFAULT_DATA, () => ({}))
+  const [data, , setValues] = Calculator.useData(DEFAULT_DATA, () => ({}))
 
   // Calculations.
   let totalSilver = 0
   let totalAK = 0
   let levelsClimbed = 0
-  let endCondition = () => (data.fromLevel + levelsClimbed < data.toLevel)
+  let endCondition = () => data.fromLevel + levelsClimbed < data.toLevel
   if (data.mode === "silver") {
     const toSilver = data.toSilverUnit === "B" ? data.toSilver * 1000 : data.toSilver
-    endCondition = () => (totalSilver + ((data.fromLevel + levelsClimbed + 1) * 50) - 1 < toSilver)
+    endCondition = () => totalSilver + (data.fromLevel + levelsClimbed + 1) * 50 - 1 < toSilver
   } else if (data.mode === "ak") {
-    endCondition = () => (totalAK + 99 < data.toAK)
+    endCondition = () => totalAK + 99 < data.toAK
   }
   while (endCondition()) {
     levelsClimbed += 1
@@ -50,56 +47,107 @@ export default () => {
   const endLevel = data.fromLevel + levelsClimbed
   const mmsRequired = endLevel <= 100 ? 0 : Math.ceil((endLevel - 100) / 4)
 
-  return <Calculator pageTitle="Tower Calculator" valueSetter={setValues}>
-    <Input.Text
-      id="fromLevel"
-      label="Current Level"
-      placeholder={DEFAULT_DATA.fromLevel.toString()}
-      pattern="^\d{1,4}$"
-      type="number"
-    />
-    <Input id="mode" label="Mode">
-      <ButtonGroup aria-label="Mode select buttons">
-        <Input.RadioButton value="level" label="To Level" data={data.mode} />
-        <Input.RadioButton value="silver" label="Available Silver" data={data.mode} />
-        <Input.RadioButton value="ak" label="Available AK" data={data.mode} />
-      </ButtonGroup>
-    </Input>
-    {data.mode === "level" && <Input.Text
-      key="toLevel"
-      id="toLevel"
-      label="Goal Level"
-      placeholder={DEFAULT_DATA.toLevel.toString()}
-      pattern="^\d{1,4}$"
-      type="number"
-    />}
-    {data.mode === "silver" && <Input.Text
-      key="toSilver"
-      id="toSilver"
-      label="Available Silver"
-      placeholder={DEFAULT_DATA.toSilver.toString()}
-      pattern="^[0-9.]+$"
-      type="number"
-      after={
-        <>
-          <input type="radio" className="btn-check" name="toSilverUnit" id="toSilverUnit-b" autoComplete="off" value="B" defaultChecked={data.toSilverUnit === "B"} />
-          <label className="btn btn-outline-secondary" htmlFor="toSilverUnit-b">B</label>
-          <input type="radio" className="btn-check" name="toSilverUnit" id="toSilverUnit-m" autoComplete="off" value="M" defaultChecked={data.toSilverUnit === "M"} />
-          <label className="btn btn-outline-secondary" htmlFor="toSilverUnit-m">M</label>
-        </>
-      } />}
-    {data.mode === "ak" && <Input.Text
-      key="toAK"
-      id="toAK"
-      label="Available AK"
-      placeholder={DEFAULT_DATA.toAK.toString()}
-      pattern="^\d+$"
-      type="number"
-    />}
+  return (
+    <Calculator pageTitle="Tower Calculator" valueSetter={setValues}>
+      <Input.Text
+        id="fromLevel"
+        label="Current Level"
+        placeholder={DEFAULT_DATA.fromLevel.toString()}
+        pattern="^\d{1,4}$"
+        type="number"
+      />
+      <Input id="mode" label="Mode">
+        <ButtonGroup aria-label="Mode select buttons">
+          <Input.RadioButton value="level" label="To Level" data={data.mode} />
+          <Input.RadioButton value="silver" label="Available Silver" data={data.mode} />
+          <Input.RadioButton value="ak" label="Available AK" data={data.mode} />
+        </ButtonGroup>
+      </Input>
+      {data.mode === "level" && (
+        <Input.Text
+          key="toLevel"
+          id="toLevel"
+          label="Goal Level"
+          placeholder={DEFAULT_DATA.toLevel.toString()}
+          pattern="^\d{1,4}$"
+          type="number"
+        />
+      )}
+      {data.mode === "silver" && (
+        <Input.Text
+          key="toSilver"
+          id="toSilver"
+          label="Available Silver"
+          placeholder={DEFAULT_DATA.toSilver.toString()}
+          pattern="^[0-9.]+$"
+          type="number"
+          after={
+            <>
+              <input
+                type="radio"
+                className="btn-check"
+                name="toSilverUnit"
+                id="toSilverUnit-b"
+                autoComplete="off"
+                value="B"
+                defaultChecked={data.toSilverUnit === "B"}
+              />
+              <label className="btn btn-outline-secondary" htmlFor="toSilverUnit-b">
+                B
+              </label>
+              <input
+                type="radio"
+                className="btn-check"
+                name="toSilverUnit"
+                id="toSilverUnit-m"
+                autoComplete="off"
+                value="M"
+                defaultChecked={data.toSilverUnit === "M"}
+              />
+              <label className="btn btn-outline-secondary" htmlFor="toSilverUnit-m">
+                M
+              </label>
+            </>
+          }
+        />
+      )}
+      {data.mode === "ak" && (
+        <Input.Text
+          key="toAK"
+          id="toAK"
+          label="Available AK"
+          placeholder={DEFAULT_DATA.toAK.toString()}
+          pattern="^\d+$"
+          type="number"
+        />
+      )}
 
-    {data.mode !== "level" && <Input.Text id="level" label="Gets To Level" disabled={true} value={(data.fromLevel + levelsClimbed).toLocaleString()} />}
-    <Input.Text id="silver" label="Silver Used" disabled={true} value={displaySilver.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 2 }) + displaySilverUnit} />
-    <Input.Text id="ak" label="AK Used" disabled={true} value={totalAK.toLocaleString()} />
-    <Input.Text id="mms" label="Mega-Masteries Required" disabled={true} value={mmsRequired.toLocaleString()} />
-  </Calculator>
+      {data.mode !== "level" && (
+        <Input.Text
+          id="level"
+          label="Gets To Level"
+          disabled={true}
+          value={(data.fromLevel + levelsClimbed).toLocaleString()}
+        />
+      )}
+      <Input.Text
+        id="silver"
+        label="Silver Used"
+        disabled={true}
+        value={
+          displaySilver.toLocaleString(undefined, {
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2,
+          }) + displaySilverUnit
+        }
+      />
+      <Input.Text id="ak" label="AK Used" disabled={true} value={totalAK.toLocaleString()} />
+      <Input.Text
+        id="mms"
+        label="Mega-Masteries Required"
+        disabled={true}
+        value={mmsRequired.toLocaleString()}
+      />
+    </Calculator>
+  )
 }
