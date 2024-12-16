8061901948fd49e754627dea589319f5a4bad983
Noah Kantrowitz <noah@coderanger.net>
2024-12-16 15:06:56 -0800

    🎨 Fix URL.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -172,7 +172,7 @@ const IndexPage = ({
                 },
                 {
                   name: "Exploring",
-                  image: "/img/items/6075.PNG",
+                  image: "/img/items/6075.png",
                   href: "/exploring/",
                 },
                 { name: "Fishing", image: "/img/items/7783.png", href: "/fishing/" },
